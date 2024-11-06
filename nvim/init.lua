196b0136d20f8151301f2adbae98e6f86a35bc14
Abdul Alim Mahir <136636635+AbdulAlimMahir@users.noreply.github.com>
2024-11-06 23:56:14 +0600

    Alphabetical keymaps and working theme_picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,12 +5,15 @@ require("mahir.plugins")
 -- Loads after everything
 require("mahir.lazy")
 
+require("mahir.theme_picker").load_last_theme()
 -- FILE STRUCTURe [nvim]
 -- -> init.lua [loads -> mahir.plugins, mahir.options, mahir.keymaps]
 -- -> Lua
 --   -> Mahir
 --     -> Plugins [contains -> all plugins]
+--     -> Themes [contains -> all themes]
 --     -> keymaps.lua
+--     -> theme_picker.lua
 --     -> lazy.lua
 --     -> options.lua
---     -> plugins.lua [loads -> Plugins]
+--     -> plugins.lua [loads -> single-line plugins]
